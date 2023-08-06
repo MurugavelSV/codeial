6c27fbf0e3946651707a7f67d0819f0da7c8a868
Murugavel V <muruvel19.10@gmail.com>
2023-08-06 20:06:46 +0530

    create router and import it at starting point file

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+app.use('/', require('./routes/index.js'));
+
+app.listen(port, (err) => {
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is up and running at port: ${port}`);
+});
