4ff75dd074f51d6a32c46a1618136f3a027f8631
Murugavel V <muruvel19.10@gmail.com>
2023-08-07 01:58:49 +0530

    create view for home

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8000;
 const app = express();
 app.use('/', require('./routes/index.js'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, (err) => {
     if(err){
         console.log(`Error in running the server: ${err}`);
