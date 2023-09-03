e79f4ffd73263429214b376d6a4cc53797628c7e
Murugavel V <muruvel19.10@gmail.com>
2023-09-03 15:15:33 +0530

    create delete post for user to delete only posts made by them and they are verified

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.createPost = (req, res) => {
     (async () => {
@@ -13,3 +14,18 @@ module.exports.createPost = (req, res) => {
         });
     })();
 };
+
+module.exports.destroyPost = (req, res) => {
+    (async () => {
+        try{
+            const post = await Post.findById(req.params.id);
+            if(post){
+                await Post.deleteOne({_id: req.params.id});
+                await Comment.deleteMany({post: req.params.id});
+            }
+            return res.redirect('back');
+        }catch(err){
+            console.log(`Error: ${err.message}`);
+        }
+    })();
+}
--- routes/post.js
+++ routes/post.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const postController = require('../controllers/post_controller');
 
 router.post('/create', passport.checkAuthenticated, postController.createPost);
+router.get('/delete/:id', passport.checkAuthenticated, postController.destroyPost);
 
 module.exports = router;
